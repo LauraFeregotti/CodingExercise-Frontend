3e2388219a667cb603a69863f893897e76deaf59
unknown <laura.feregotti@gmail.com>
2019-06-13 11:25:51 +0200

    Adding query params to get just 10 messages newer and polishing my code

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,17 +5,14 @@ let textbox = document.getElementById("textbox");
 let button = document.getElementById("button");
 
 /* Setting APIs to a variable*/
-let apiUrlForGet = 'http://localhost:5000/api/v1.0?token=';
-let url = 'http://localhost:5000/api/v1.0';
-let author = 'Tom';
-let token = 'laura-test';
+let url = new URL("http://localhost:5000/api/v1.0");
 
 /* Creating prompt messages, so the user can set the value of the token(chatroom) and the author(nick/name) */
-var chatroom = prompt("Enter the name of the chatroom");
+let chatroom = prompt("Enter the name of the chatroom");
 if (chatroom != null) {
   token = chatroom;
 }
-var nick = prompt("Enter your nick");
+let nick = prompt("Enter your nick");
 if (nick != null) {
   author = nick;
 }
@@ -23,7 +20,8 @@ if (nick != null) {
 /*Creating the POST request */
 
 function sendData() {
-
+  url.searchParams.delete("token", token);
+  url.searchParams.delete("limit", 10);
     response = fetch(url, {
       method: 'POST',
       body: JSON.stringify({
@@ -48,7 +46,10 @@ function sendData() {
 
 /*Creating the GET request */
 function getMessages() {
-    endpoint = `${apiUrlForGet}${token}`;
+    /*Adding query params to get just 10 messages newer */
+  url.searchParams.append("token", token);
+  url.searchParams.append("limit", 10);
+    endpoint = `${url}`;
     fetch(endpoint)
       .then(function (response) {
         return response.json();
@@ -85,4 +86,45 @@ function getMessages() {
       });
 };
 
-setInterval(getMessages, 5000); 
+setInterval(polling, 5000); 
+
+function polling() {
+  /*Adding query params to get just 10 messages newer */
+  url.searchParams.append("token", token);
+  url.searchParams.append("limit", 10);
+  endpoint = `${url}`;
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
+      messages.innerHTML = "";
+      for (let index = 0; index < myJson.length; index++) {
+        let container = document.createElement("div");
+        container.className = 'col-lg-12 col-sm-12 col-xs-12 container ';
+        let secondcontainer = document.createElement("div");
+        secondcontainer.className = 'secondcontainer row';
+        let newMessage = document.createElement("p");
+        newMessage.className = 'messagedisplay';
+        let newMessageAuthor = document.createElement("p");
+        newMessageAuthor.className = 'author';
+        let newMessageTime = document.createElement("p");
+        newMessageTime.className = 'time';
+      
+        let timeforparsing = myJson[index].time;
+        let parsing = moment(timeforparsing).format("DD MMM YYYY hh:mm a");
+        console.log(parsing);
+        newMessageAuthor.innerHTML= myJson[index].author;
+        newMessageTime.innerHTML= parsing
+        newMessage.innerHTML = myJson[index].message; 
+        secondcontainer.appendChild(newMessageAuthor);
+        secondcontainer.appendChild(newMessage);
+        secondcontainer.appendChild(newMessageTime);
+        container.appendChild(secondcontainer);
+        messages.appendChild(container);
+        
+
+      }
+    });
+}; 
