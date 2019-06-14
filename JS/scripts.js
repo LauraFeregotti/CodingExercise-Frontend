690c05d95615f2392f85e883a7f18200790d980e
unknown <laura.feregotti@gmail.com>
2019-06-14 02:26:21 +0200

    Polishing my code

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,14 @@
-/* Getting my HTML elements and assigning them variables for JavaScript manipulation*/
+// Getting my HTML elements and assigning them variables for JavaScript manipulation
 let listofelements = document.getElementById("listofelements");
 let messages = document.getElementById("messages");
 let textbox = document.getElementById("textbox");
 let button = document.getElementById("button");
 
-/* Setting APIs to a variable*/
+// Setting APIs to a variable
 let url = new URL("http://localhost:5000/api/v1.0");
 
-/* Creating prompt messages, so the user can set the value of the token(chatroom) and the author(nick/name) */
+/* Creating prompt messages, so the user can set the value of the token(chatroom) and the author(nick/name)
+Im currently using the chatroom as a token as I never receive the token for my API */
 let chatroom = prompt("Enter the name of the chatroom");
 if (chatroom != null) {
   token = chatroom;
@@ -16,80 +17,35 @@ let nick = prompt("Enter your nick");
 if (nick != null) {
   author = nick;
 }
-
-/*Creating the POST request */
-
+//Creating the POST request 
 function sendData() {
   url.searchParams.delete("token", token);
   url.searchParams.delete("limit", 10);
-    response = fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({
-        author: author,
-        message: textbox.value
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'token': token
-      }
-    }).then(response => {
-      if (response.ok) getMessages()
-    });
-  }
-  
-  button.addEventListener('click', sendData);
-  textbox.addEventListener('keyup', function onEvent(e) {
-    if (e.keyCode === 13) {
-        sendData();
+  response = fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      author: author,
+      message: textbox.value
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'token': token
     }
+  }).then(response => {
+    if (response.ok) getMessages()
+    textbox.value = "";
+  });
+}
+//Every time the user clicks the button or uses the "enter" key, the function "sendData" fires and the POST request is made.
+button.addEventListener('click', sendData);
+textbox.addEventListener('keyup', function onEvent(e) {
+  if (e.keyCode === 13) {
+    sendData();
+  }
 });
-
-/*Creating the GET request */
+//Creating the GET request 
 function getMessages() {
-    /*Adding query params to get just 10 messages newer */
-  url.searchParams.append("token", token);
-  url.searchParams.append("limit", 10);
-    endpoint = `${url}`;
-    fetch(endpoint)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (myJson) {
-        console.log(myJson);
-        messages.innerHTML = "";
-        for (let index = 0; index < myJson.length; index++) {
-          let container = document.createElement("div");
-        container.className = 'col-lg-12 col-sm-12 col-xs-12 container ';
-        let secondcontainer = document.createElement("div");
-        secondcontainer.className = 'secondcontainer row';
-        let newMessage = document.createElement("p");
-        newMessage.className = 'messagedisplay';
-        let newMessageAuthor = document.createElement("p");
-        newMessageAuthor.className = 'author';
-        let newMessageTime = document.createElement("p");
-        newMessageTime.className = 'time';
-/*Parsing time using Moment*/
-        let timeforparsing = myJson[index].time;
-        let parsing = moment(timeforparsing).format("DD MMM YYYY hh:mm a");
-        console.log(parsing);
-        newMessageAuthor.innerHTML= myJson[index].author;
-        newMessageTime.innerHTML= parsing
-        newMessage.innerHTML = myJson[index].message; 
-        secondcontainer.appendChild(newMessageAuthor);
-        secondcontainer.appendChild(newMessage);
-        secondcontainer.appendChild(newMessageTime);
-        container.appendChild(secondcontainer);
-        messages.appendChild(container);
-          textbox.value = "";
-  
-        }
-      });
-};
-
-setInterval(polling, 5000); 
-
-function polling() {
-  /*Adding query params to get just 10 messages newer */
+  //Adding query params to get just 10 messages newer 
   url.searchParams.append("token", token);
   url.searchParams.append("limit", 10);
   endpoint = `${url}`;
@@ -98,33 +54,40 @@ function polling() {
       return response.json();
     })
     .then(function (myJson) {
-      console.log(myJson);
       messages.innerHTML = "";
-      for (let index = 0; index < myJson.length; index++) {
-        let container = document.createElement("div");
-        container.className = 'col-lg-12 col-sm-12 col-xs-12 container ';
-        let secondcontainer = document.createElement("div");
-        secondcontainer.className = 'secondcontainer row';
-        let newMessage = document.createElement("p");
-        newMessage.className = 'messagedisplay';
-        let newMessageAuthor = document.createElement("p");
-        newMessageAuthor.className = 'author';
-        let newMessageTime = document.createElement("p");
-        newMessageTime.className = 'time';
-      
-        let timeforparsing = myJson[index].time;
-        let parsing = moment(timeforparsing).format("DD MMM YYYY hh:mm a");
-        console.log(parsing);
-        newMessageAuthor.innerHTML= myJson[index].author;
-        newMessageTime.innerHTML= parsing
-        newMessage.innerHTML = myJson[index].message; 
-        secondcontainer.appendChild(newMessageAuthor);
-        secondcontainer.appendChild(newMessage);
-        secondcontainer.appendChild(newMessageTime);
-        container.appendChild(secondcontainer);
-        messages.appendChild(container);
-        
-
-      }
+      creatingMessage(myJson);
     });
-}; 
+};
+//This function takes the json response values and creates the message
+function creatingMessage(myJson) {
+  for (let index = 0; index < myJson.length; index++) {
+//Creating HTML elements
+    let messageContainer = document.createElement("div");
+    messageContainer.className = 'secondcontainer row';
+
+    let message = document.createElement("p");
+    message.className = 'messagedisplay';
+
+    let author = document.createElement("p");
+    author.className = 'author';
+
+    let dateAndTime = document.createElement("p");
+    dateAndTime.className = 'time';
+    //Parsing time using Moment
+    let timeforparsing = myJson[index].time;
+    let parsing = moment(timeforparsing).format("DD MMM YYYY hh:mm a");
+//Assigning to every HTML element an specific jason value
+    author.innerHTML = myJson[index].author;
+    dateAndTime.innerHTML = parsing;
+    message.innerHTML = myJson[index].message;
+//Appending the HTML elements and containers
+    messageContainer.appendChild(author);
+    messageContainer.appendChild(message);
+    messageContainer.appendChild(dateAndTime);
+    messages.appendChild(messageContainer);
+
+  }
+}
+//This method generates a polling. Every 5 seconds it will generate a GET request
+setInterval(getMessages, 5000);
+
---
+++ README.md
@@ -0,0 +1,19 @@
+###Coding Exercise
+
+## Prerequisites
+Install Node.js  you can get it from [here] https://nodejs.org/en/
+
+##Set-up
+1. Clone the repository of the project.
+2. As the API wasn't working, my boyfriend created an API. So you will have to follow the instructions on this [repository] https://github.com/Av4t4r/chattr-box
+
+##Run it
+Once the API is running, just run the local server:
+```
+http-server
+
+```
+After that, you would be able to access to the project via **http://localhost:8080/index.html** 
+
+
+
